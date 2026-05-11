d2a189480bcd3e0063d352d01e50a6cde1738570
KrutalevichLev <krutaleviclev@gmail.com>
2026-05-11 17:19:27 +0300

    task02 work

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -6,6 +6,41 @@
 // расположенных на главной и побочной диагоналях.
 #include "logic.h"
 
+int find_sum_of_main_diagonales(int** matrix, int n) {
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += *(*(matrix + i) + i);
+	}
+
+	return sum;
+}
+
+int find_sum_of_second_diagonales(int** matrix, int n) {
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += *(*(matrix + i) + n - 1 - i);
+	}
+
+	return sum;
+}
+
 int sum_main_and_second_diagonales_elements(int** matrix, int n) {
-	return 0;
+	if (matrix == nullptr) {
+		return 0;
+	}
+	if (n <= 0) {
+		return 0;
+	}
+
+	int sum = find_sum_of_main_diagonales(matrix, n);
+	sum += find_sum_of_second_diagonales(matrix, n);
+	if (n % 2 != 0) {
+		sum -= *(*(matrix + n / 2) + n / 2);
+	}
+
+	return sum;
 }
