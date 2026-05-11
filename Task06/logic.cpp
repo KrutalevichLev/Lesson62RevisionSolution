fea2da3dce1e5fe7dd7dc58c740cde5d308b14f8
KrutalevichLev <krutaleviclev@gmail.com>
2026-05-11 18:38:15 +0300

    task 06

--- Task06/logic.cpp
+++ Task06/logic.cpp
@@ -6,8 +6,120 @@
 // находит значение локального максимума, который является
 // минимальным среди всех локальных максимумов. 
 
+//#include "logic.h"
+//
+//int* find_local_maximum(int** matrix, int n, int m) {
+//
+//
+//
+//	for (int i = 1; i < n - 1; i++)
+//	{
+//		for (int j = 1; j < m - 1; j++)
+//		{
+//			if ((*(*matrix + j) + i) > (*(*matrix + j - 1) + i) &&
+//				(*(*matrix + j) + i) > (*(*matrix + j + 1) + i) &&
+//				(*(*matrix + j) + i) > (*(*matrix + j) + i - 1) &&
+//				(*(*matrix + j) + i) > (*(*matrix + j) + i + 1)) {
+//
+//			}
+//		}
+//	}
+//
+//	for (int j = 1; j < m - 1; j++)
+//	{
+//		if ((*(*matrix + j)) > (*(*matrix + j + 1)) &&
+//			(*(*matrix + j)) > (*(*matrix + j - 1)) &&
+//			(*(*matrix + j)) > (*(*matrix + j) + 1)) {
+//
+//		}
+//	}
+//
+//	for (int j = 1; j < m - 1; j++)
+//	{
+//		if ((*(*matrix + j)) > (*(*matrix + j + 1)) &&
+//			(*(*matrix + j)) > (*(*matrix + j - 1)) &&
+//			(*(*matrix + j)) > (*(*matrix + j) + n - 1)) {
+//
+//		}
+//	}
+//
+//	for (int i = 1; i < n - 1; i++)
+//	{
+//		if ((*(*matrix) + i) > (*(*matrix) + i - 1) &&
+//			(*(*matrix) + i) > (*(*matrix + 1) + i) &&
+//			(*(*matrix) + i) > (*(*matrix) + i + 1)) {
+//
+//		}
+//	}
+//
+//	for (int i = 1; i < n - 1; i++)
+//	{
+//		if ((*(*matrix) + i) > (*(*matrix) + i - 1) &&
+//			(*(*matrix) + i) > (*(*matrix - 1) + i) &&
+//			(*(*matrix) + i) > (*(*matrix) + i + 1)) {
+//
+//		}
+//	}
+//
+//	if ((*(*matrix)) > (*(*matrix) + 1) &&
+//		(*(*matrix)) > (*(*matrix + 1))) {
+//
+//	}
+//
+//	if ((*(*matrix) + n) > (*(*matrix + n - 1)) &&
+//		(*(*matrix) + n) > (*(*matrix + n + 1))) {
+//
+//	}
+//
+//	if ((*(*matrix)) > (*(*matrix) + 1) &&
+//		(*(*matrix)) > (*(*matrix ) +1)) {
+//
+//	}
+//
+//	if ((*(*matrix) + n) > (*(*matrix + n - 1)) &&
+//		(*(*matrix) + n) > (*(*matrix + n + 1))) {
+//
+//	}
+//
+//}
+//
+//int get_min_maximum(int** matrix, int n, int m) {
+//	return 0;
+//}
+//
+//
 #include "logic.h"
 
 int get_min_maximum(int** matrix, int n, int m) {
-	return 0;
+	int min_max = 2147483647;
+	bool found = false;
+
+
+	if (n < 3 || m < 3) {
+		return -0;
+	}
+
+	for (int i = 1; i < n - 1; i++) {
+		for (int j = 1; j < m - 1; j++) {
+			int current = matrix[i][j];
+
+			if (current > matrix[i - 1][j - 1] &&
+				current > matrix[i - 1][j] &&
+				current > matrix[i - 1][j + 1] &&
+				current > matrix[i][j - 1] &&
+				current > matrix[i][j + 1] &&
+				current > matrix[i + 1][j - 1] &&
+				current > matrix[i + 1][j] &&
+				current > matrix[i + 1][j + 1])
+			{
+
+				if (current < min_max) {
+					min_max = current;
+					found = true;
+				}
+			}
+		}
+	}
+
+	return found ? min_max : -0;
 }
